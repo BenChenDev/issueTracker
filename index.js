7b46d6a8d7028778ca621d4d6dcb11006562568f
TRUZijunChen <benjamin9527@icloud.com>
2019-12-28 20:35:24 -0800

    change package.json npm command sctipt

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+
+//import models
+require('./models/Product');
+
+
 const app = express();
+
+//create admin for DB
+var mongoPort    = '27017'
+var mongoHost    = 'localhost'
+
+var dbName       = 'issueTracker'
+//var userName     = 'admin'
+//var userPassword = 'admin'
 
 //connect to database
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:7000/mongooseDB', 
+mongoose.connect('mongodb://'+ mongoHost + ':' + mongoPort + '/' + dbName, 
 {
     useUnifiedTopology:true, 
     useNewUrlParser:true
@@ -20,10 +33,23 @@ db.once('open', () => {
 
 app.use(bodyParser.json());
 
+//import routes
+require('./routers/productRoute')(app);
+
+//configue project production
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    const path = require('path');
+    app.get('*',(req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+    })
+}
+
 //set port to 7000 if we dont have default port yet
 const port = process.env.PORT || 5000;
 
-//send request
+//test send request
 app.get('/', (req,res)=>{
     res.render('index');
 });
