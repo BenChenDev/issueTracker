7cde234b80bc3f2a2cb38fae519a3ebcaab3a01a
TRUZijunChen <chenbenjamin66@gmail.com>
2020-08-13 15:31:01 -0700

    edited listDetailView

--- backend/issueManager/urls.py
+++ backend/issueManager/urls.py
@@ -4,6 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('', include('IssueTracker.urls')),
-    
+    path('issue_tracker/', include('IssueTracker.urls'))
 ]
--- frontend/issue-tracker/src/containers/IssueDetailView.js
+++ frontend/issue-tracker/src/containers/IssueDetailView.js
@@ -1,28 +1,68 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-import { Card } from 'antd';
+import { Card, Button, Tooltip, PageHeader, Tabs, Statistic, Descriptions} from 'antd';
+import { EditOutlined } from '@ant-design/icons';
 
 class IssueDetail extends Component {
   
   state = {
     issue:[]
   }
-  
+
   componentDidMount() {
     const id = this.props.match.params.issueID;
-    axios.get(`http://127.0.0.1:8000/api/issues/${id}`)
+    axios.get(`http://127.0.0.1:8000/issue_tracker/api/issues/${id}`)
       .then(res => {
         this.setState({
           issue: res.data
         });
       })
-  }
+  };
 
   render() {
+    const { TabPane } = Tabs
+    const title = this.state.issue.title ? this.state.issue.title : ""
+    const creator = this.state.issue.creator ? this.state.issue.creator : ""
+    const description = this.state.issue.description ? this.state.issue.description : ""
+    const createTime = this.state.issue.created ? this.state.issue.created : ""
+    const modified = this.state.issue.modified ? this.state.issue.modified : ""
+
+    const creationInfor = (column = 3) => (
+      <Descriptions size="small" column={column}>
+        <Descriptions.Item label="Created by">{creator}</Descriptions.Item>
+        <Descriptions.Item label="Creation Time">{createTime}</Descriptions.Item>
+        <Descriptions.Item label="Last Modified">{modified}</Descriptions.Item>
+      </Descriptions>
+    )
+
+    const Content = ({ children, extra }) => {
+      return (
+        <div className="content">
+          <div className="main">{children}</div>
+          <div className="extra">{extra}</div>
+        </div>
+      );
+    }
+
     return (
-      <Card title={this.state.issue.title}>
-        <p>{this.state.issue.description}</p>
-      </Card>
+      <PageHeader
+        className="site-page-header-responsive"
+        onBack={() => window.history.back()}
+        title={title}
+        extra={[
+          <Tooltip title="edit">
+            <Button type="primary" icon={<EditOutlined/>}>Edit</Button>
+          </Tooltip>
+        ]}
+        footer={
+          <Tabs defaultActiveKey="1">
+            <TabPane tab="Description" key="1">{description}</TabPane>
+            <TabPane tab="Comment" key="2" />
+          </Tabs>
+        }
+      >
+        <Content>{creationInfor()}</Content>
+      </PageHeader>
     )
   }
 }
